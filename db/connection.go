8530d1a57ccb9f0912da90989ee0d5cdd73b36a6
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-01 14:22:56 -0300

    config: connection db postgres

---
+++ db/connection.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql",
+	"fmt",
+	"github.com/RobsonFeitosa/app-crud-go-lang/config"
+)
+
+func OpenConnection() (*sql.DB, error) {
+	conf := configs.GetDB()
+
+	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
+		config.Host,
+		config.Port,
+		config.User,
+		config.Pass,
+		config.Database
+	)
+
+	conn, err := sql.Open("postgres", sc)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = conn.Ping()
+
+	return conn, err
+}
--- go.mod
+++ go.mod
@@ -3,5 +3,5 @@ module exemplo
 go 1.18
 
 require ( 
-    github.com:RobsonFeitosa/app-crud-go-lang/ v1.0.0 
+    github.com/RobsonFeitosa/app-crud-go-lang/ @v1.0.0
 )
